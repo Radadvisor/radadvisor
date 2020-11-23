bd3521316aba28b798714d5fc77b38802d3e21c9
Felix <felix@FelixWildesMBP.fritz.box>
2020-11-23 12:00:25 +0100

    Inital comit

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-#import django_heroku
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,7 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 #Activate Django-Heroku
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # Static files (CSS, JavaScript, Images)
