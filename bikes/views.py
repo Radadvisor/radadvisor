9a852b2df18fe7bea2e3967ec0441c029241395d
Felix <felix@FelixWildesMBP.fritz.box>
2020-11-26 19:21:27 +0100

    Home adaptiert;Alle Frage

--- bikes/views.py
+++ bikes/views.py
@@ -18,3 +18,45 @@ def strasse(request):
 
 def stadt(request):
     return render(request, 'stadt.html')
+
+def wald(request):
+    return render(request, 'wald.html')
+
+def bergig(request):
+    return render(request, 'bergig.html')
+
+def strasse(request):
+    return render(request, 'strasse.html')
+
+def stadt(request):
+    return render(request, 'stadt.html')
+
+def electric(request):
+    return render(request, 'electric.html')
+
+def ebike(request):
+    return render(request, 'ebike.html')
+
+def waldsportlich(request):
+    return render(request, 'waldsportlich.html')
+
+def waldnormal(request):
+    return render(request, 'waldnormal.html')
+
+def strassesportlich(request):
+    return render(request, 'strassesportlich.html')
+
+def strassenormal(request):
+    return render(request, 'strassenormal.html')
+
+def stadtfalten(request):
+    return render(request, 'stadtfalt.html')
+
+def stadtnormal(request):
+    return render(request, 'stadtnormal.html')
+
+def bergsprung(request):
+    return render(request, 'bergigsprung.html')
+
+def bergnormal(request):
+    return render(request, 'bergignormal.html')
--- djangoherokuapp/urls.py
+++ djangoherokuapp/urls.py
@@ -4,20 +4,31 @@ from django.urls import path
 from home.views import homepage
 from kontakt.views import kontakt
 from bewertung.views import bewertunglist, addbewertung, deletebewertung
-from bikes.views import bikes, wald, strasse, stadt, bergig
+from bikes.views import bikes, wald, strasse, bergig, stadt, electric, ebike, waldsportlich, waldnormal, strassenormal, strassesportlich, stadtfalten, stadtnormal, bergnormal, bergsprung
+
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', homepage),
     path('', homepage),
+    path('home/', homepage),
+    path('admin/', admin.site.urls),
+    path('noelectric/bikes/', bikes),
+    path('noelectric/bikes/wald/', wald),
+    path('noelectric/bikes/wald/sportlich/', waldsportlich),
+    path('noelectric/bikes/wald/normal/', waldnormal),
+    path('noelectric/bikes/strasse/', strasse),
+    path('noelectric/bikes/strasse/sportlich/', strassesportlich),
+    path('noelectric/bikes/strasse/normal/', strassenormal),
+    path('noelectric/bikes/stadt/', stadt),
+    path('noelectric/bikes/stadt/falten/', stadtfalten),
+    path('noelectric/bikes/stadt/normal/', stadtnormal),
+    path('noelectric/bikes/bergig/', bergig),
+    path('noelectric/bikes/bergig/sprung/', bergsprung),
+    path('noelectric/bikes/bergig/normal/', bergnormal),
+    path('electric/', electric),
     path('kontakt/', kontakt),
-    path('bikes/', bikes),
-    path('bikes/wald/', wald),
-    path('bikes/strasse/', strasse),
-    path('bikes/stadt/', stadt),
-    path('bikes/bergig/', bergig),
+    path('electric/ebike/', ebike),
     path('bewertung/', bewertunglist),
     path('addbewertung/', addbewertung),
-    path('deletebewertung/<int:bewertung_id>/', deletebewertung)
+    path('deletebewertung/<int:bewertung_id>/', deletebewertung),
 ]
