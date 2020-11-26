c4339661addc08cffe1a273fd8d69f4d64156f89
Felix <felix@FelixWildesMBP.fritz.box>
2020-11-26 13:50:29 +0100

    static_root

--- djangoherokuapp/settings.py
+++ djangoherokuapp/settings.py
@@ -148,8 +148,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 
